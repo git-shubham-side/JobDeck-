9bfadde8dd33cb64a8b89d25826210bb70287a5a
Shubh <rathodshubham7711@gmail.com>
2026-06-26 13:46:52 +0530

    Server configuration pending

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv").config();
 
-app.listen(3000, (req, res) => {
-  console.log("Server Started");
+app.get("/", (req, res) => {
+  res.json("Server is listening on *");
 });
+
+app.listen(3000, () => {
+  console.log("server is running");
+});
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,7 @@
+const app = require("./app");
+
+//SERVER CONFIGURATION PENDING
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is Listening on http://localhost:300`);
+});
