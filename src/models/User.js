f7d017304bc9c426939db5f0a73cc7f38bd7c3c8
Shubh <rathodshubham7711@gmail.com>
2026-07-11 03:52:39 +0530

    model is ready for rg/login

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const connect = require("../DB Connection/db");
-const { type } = require("os");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 connect();
 
@@ -8,17 +9,17 @@ const UserSchema = mongoose.Schema(
   {
     firstName: {
       type: String,
-      required: true,
+      required: [true, "Please enter first name"],
       trim: true,
       lowercase: true,
       uppercase: false,
       minlength: 2,
       maxlength: 50,
-      match: /^[A-Za-z]+$/,
+      match: /^[A-Za-z ]+$/,
     },
     lastName: {
       type: String,
-      required: true,
+      required: [true, "Please enter last name"],
       trim: true,
       lowercase: true,
       uppercase: false,
@@ -28,22 +29,22 @@ const UserSchema = mongoose.Schema(
     },
     email: {
       type: String,
-      required: true,
+      required: [true, "Please enter an email address."],
       unique: true,
       trim: true,
       lowercase: true,
 
       validate: {
-        validator: function checkEmail(email) {
-          return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
-        },
+        // Function to check the email
+        validator: validator.isEmail,
         message: "Please enter a valid email address",
       },
     },
     password: {
       type: String,
-      required: true,
+      required: [true, "Please enter a password"],
       minlength: 8,
+      select: false,
     },
     terms: {
       type: Boolean,
@@ -57,12 +58,28 @@ const UserSchema = mongoose.Schema(
     },
     profileUrl: {
       type: String,
-      match: /^https?:\/\/.+/,
+      validate: validator.isURL,
+      message: "Please enter a valid URL!",
     },
   },
-  { timeStamps: true },
+  { timestamps: true },
 );
 
+//Pre Hook for Password Hashing
+UserSchema.pre("save", async function () {
+  //Check the password is modified in last save
+  if (!this.isModified("password")) {
+    return;
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+//Method to check the password
+UserSchema.methods.checkPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+//User Regestration Model
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
