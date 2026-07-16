42ca84d791e0538af35cf9a54b0c080a2b73fb90
Shubh <rathodshubham7711@gmail.com>
2026-07-16 15:01:39 +0530

    mvc architecture followed

--- src/app.js
+++ src/app.js
@@ -10,17 +10,14 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-//Controllers -> Signup/Register
-const {
-  registerPostController,
-  registerGetController,
-} = require("./controllers/registerController");
-
-//Controllers -> Login
-const {
-  loginGetController,
-  loginPostController,
-} = require("./controllers/loginController");
+//Login Routes
+const loginRoutes = require("./routes/Login");
+//Signup Routes
+const signupRoutes = require("./routes/Signup");
+//Dashboard Routes
+const dashboardRoutes = require("./routes/Dashboard");
+//Logout Routes
+const logoutRoutes = require("./routes/Logout");
 
 //SESSION Setup
 app.use(
@@ -52,42 +49,27 @@ app.use(express.json());
 
 //Error Handler -- Middleware
 const errorhandler = require("./middlewares/errorHandler");
-const { compareSync } = require("bcrypt");
 
 // Request Handling
 app.get(["/api/v1/landing", "/"], (req, res) => {
   res.render("Landing/landing");
 });
 
-//Login Route
-app.get("/api/v1/login", loginGetController);
-app.post("/api/v1/login", loginPostController);
+//Login --> /api/v1/login
+app.use(loginRoutes);
+
+//Signup --> /api/v1/signup
+app.use(signupRoutes);
 
 //Dasbhoard
 //GET : /api/v1/dashboard
-app.get("/api/v1/dashboard", async (req, res) => {
-  let idx = req.session.userId;
-  const user = await User.findById({ _id: idx });
-  res.render("Dashboard/dashboard", { user });
-});
-
-//Signup Routes:
-// GET : /api/v1/signup
-app.get("/api/v1/signup", registerGetController);
-// POST : /api/v1/signup
-app.post("/api/v1/signup", registerPostController);
+app.use(dashboardRoutes);
 
 //Logout Routes
-app.post("/api/v1/logout", (req, res, next) => {
-  req.session.destroy((err) => {
-    if (err) {
-      return next(err);
-    }
-    res.clearCookie("connect.sid"); // Session cookie remove
-    res.redirect("/api/v1/login");
-  });
-});
+//POST : /api/v1/logout
+app.use(loginRoutes);
 
+//Server error Handler
 app.use(errorhandler);
 
 module.exports = app;
